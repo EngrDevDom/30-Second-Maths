844d5f977c0b287c68a341448a6642da0f41b199
Dominic A. Miralles <60880034+EngrDevDom@users.noreply.github.com>
2020-09-16 01:39:34 +0800

    first commit

---
+++ 30-Second Maths/Numbers and Counting/Counting_Bases.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input('Enter a number: '))
+base = int(input('Enter the base: '))
+
+if base == 10:
+    value = num
+    print(num, 'to the base of', base, 'is equal to', value)
+
+elif base == 16:
+    value =
+
